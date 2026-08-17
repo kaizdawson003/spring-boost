bd4eb8d95f8c8cf87a73baa384a254e782ac9793
unknown <phucp9698@gmail.com>
2026-08-17 19:38:51 +0700

    fix UUID

--- src/main/java/com/example/demo/entity/StudentProfile.java
+++ src/main/java/com/example/demo/entity/StudentProfile.java
@@ -18,7 +18,7 @@ import java.util.UUID;
 @Entity
 public class StudentProfile {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String avatar;
 }
