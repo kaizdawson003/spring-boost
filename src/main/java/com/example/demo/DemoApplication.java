26c3ea6a9e311d990a5b4e255e835b33467cdb8e
unknown <phucp9698@gmail.com>
2026-08-17 19:09:35 +0700

    firt-commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableJpaAuditing
+@EnableScheduling
+@OpenAPIDefinition(info = @Info(title = "Course API", version = "1.0", description = "Information"))
+@SecurityScheme(name = "api", scheme = "bearer", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
+public class DemoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
---
+++ src/main/java/com/example/demo/controller/AuthenAPI.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.Account;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin("*")
+public class AuthenAPI {
+    @GetMapping("/api/accounts")
+    public List<Account> getAccounts(){
+        System.out.println("getAccounts");
+        return null;
+    }
+    @PostMapping("/api/account")
+    public Account createAccount(@RequestBody Account account){
+        System.out.println("createAccount");
+        return account;
+    }
+}
