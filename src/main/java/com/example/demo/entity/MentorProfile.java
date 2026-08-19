54280fafdc30d454d7ed7f38716ec7b803e372d6
unknown <phucp9698@gmail.com>
2026-08-19 14:54:19 +0700

    adding mentor profile

---
+++ src/main/java/com/example/demo/entity/MentorProfile.java
@@ -0,0 +1,23 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class MentorProfile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String major;
+}
